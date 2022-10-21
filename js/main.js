52d46e7af478c3fe81768b06ca8cfebcd110d8d3
Nicholas VanDyken <vandyken.nicholas@gmail.com>
2022-10-21 11:29:06 -0400

    testing

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 ///// Global document///////
-let div = document.createElement("div");
+
 let p = document.createElement("p");
 let x = 24;
 let y = '12';
 
-div.append(`This is the 1st div`);
-p.append(`This is the first P`);
-div.append(`this is a variable test, this is x, ${x}, this is y, ${y}.`)
+function createDiv()
+{
+	let div = document.createElement("div");
+	let node = document.createTextNode(`This is the 1st div`)
+	div.appendChild(node);
+}
+
+createDiv();
+// p.append(`This is the first P`);
+// div.append(`this is a variable test, this is x, ${x}, this is y, ${y}.`)
 
 
