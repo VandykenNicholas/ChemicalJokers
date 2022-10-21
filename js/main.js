5b75fe6fe8419d2337fb40c0c5bb94889f260df8
Nicholas VanDyken <vandyken.nicholas@gmail.com>
2022-10-21 11:18:13 -0400

    variable write test

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-let test = 'TESTING1';
-document.getElementById('test').innerHTML = test;
+///// Global document///////
+let div = document.createElement("div");
+let p = document.createElement("p");
+let x = 24;
+let y = '12';
+
+div.append(`This is the 1st div`);
+p.append(`This is the first P`);
+div.append(`this is a variable test, this is x, ${x}, this is y, ${y}.`)
+
+
