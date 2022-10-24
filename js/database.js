44626c0cc36100f9c47f80c7c2c661d817320b85
Nicholas VanDyken <vandyken.nicholas@gmail.com>
2022-10-23 22:43:37 -0400

    started the js process for the html

---
+++ js/database.js
@@ -0,0 +1,198 @@
+// <div className="game-container">
+// 	<div className="match-container">
+// 		<div className="item-match-info">Date</div>
+// 		<div className="item-match-info">Opponent: VGG</div>
+// 		<div className="item-match-info">Win</div>
+// 	</div>
+// 	<div className="stats-container">
+// 		<div className="players-container">
+// 			<div className="player-top-container">
+// 				<div className="item-player-top">Map</div>
+// 				<div className="item-player-top">Score</div>
+// 			</div>
+// 			<div className="item-player-pic">Players</div>
+// 			<div className="item-player-pic">Dutch</div>
+// 			<div className="item-player-pic">DJ</div>
+// 			<div className="item-player-pic">Fatigue</div>
+// 			<div className="item-player-pic">Slog</div>
+// 			<div className="item-player-pic">Marky</div>
+// 		</div>
+// 		<div className="map-scroll-container">
+// 			<div className="map-stats-container">
+// 				<div className="map-info-container">
+// 					<div className="item-map-info">Suna</div>
+// 					<div className="item-map-info">11-9</div>
+// 				</div>
+// 				<div className="kd-container">
+// 					<div className="item-kd-inner">K</div>
+// 					<div className="item-kd-inner">D</div>
+// 					<div className="item-kd-inner">KDR</div>
+// 					<div className="item-kd-inner">3</div>
+// 					<div className="item-kd-inner">5</div>
+// 					<div className="item-kd-inner">2.00</div>
+// 					<div className="item-kd-inner">3</div>
+// 					<div className="item-kd-inner">5</div>
+// 					<div className="item-kd-inner">2.00</div>
+// 					<div className="item-kd-inner">3</div>
+// 					<div className="item-kd-inner">5</div>
+// 					<div className="item-kd-inner">2.00</div>
+// 					<div className="item-kd-inner">3</div>
+// 					<div className="item-kd-inner">5</div>
+// 					<div className="item-kd-inner">2.00</div>
+// 					<div className="item-kd-inner">3</div>
+// 					<div className="item-kd-inner">5</div>
+// 					<div className="item-kd-inner">2.00</div>
+// 				</div>
+// 			</div>
+//
+// 			<div className="map-stats-container">
+// 				<div className="map-info-container">
+// 					<div className="item-map-info">Suna</div>
+// 					<div className="item-map-info">11-9</div>
+// 				</div>
+// 				<div className="kd-container">
+// 					<div className="item-kd-inner">K</div>
+// 					<div className="item-kd-inner">D</div>
+// 					<div className="item-kd-inner">KDR</div>
+// 					<div className="item-kd-inner">3</div>
+// 					<div className="item-kd-inner">5</div>
+// 					<div className="item-kd-inner">2.00</div>
+// 					<div className="item-kd-inner">3</div>
+// 					<div className="item-kd-inner">5</div>
+// 					<div className="item-kd-inner">2.00</div>
+// 					<div className="item-kd-inner">3</div>
+// 					<div className="item-kd-inner">5</div>
+// 					<div className="item-kd-inner">2.00</div>
+// 					<div className="item-kd-inner">3</div>
+// 					<div className="item-kd-inner">5</div>
+// 					<div className="item-kd-inner">2.00</div>
+// 					<div className="item-kd-inner">3</div>
+// 					<div className="item-kd-inner">5</div>
+// 					<div className="item-kd-inner">2.00</div>
+// 				</div>
+// 			</div>
+// 			<div className="map-stats-container">
+// 				<div className="map-info-container">
+// 					<div className="item-map-info">Suna</div>
+// 					<div className="item-map-info">11-9</div>
+// 				</div>
+// 				<div className="kd-container">
+// 					<div className="item-kd-inner">K</div>
+// 					<div className="item-kd-inner">D</div>
+// 					<div className="item-kd-inner">KDR</div>
+// 					<div className="item-kd-inner">3</div>
+// 					<div className="item-kd-inner">5</div>
+// 					<div className="item-kd-inner">2.00</div>
+// 					<div className="item-kd-inner">3</div>
+// 					<div className="item-kd-inner">5</div>
+// 					<div className="item-kd-inner">2.00</div>
+// 					<div className="item-kd-inner">3</div>
+// 					<div className="item-kd-inner">5</div>
+// 					<div className="item-kd-inner">2.00</div>
+// 					<div className="item-kd-inner">3</div>
+// 					<div className="item-kd-inner">5</div>
+// 					<div className="item-kd-inner">2.00</div>
+// 					<div className="item-kd-inner">3</div>
+// 					<div className="item-kd-inner">5</div>
+// 					<div className="item-kd-inner">2.00</div>
+// 				</div>
+// 			</div>
+// 		</div>
+// 		<div className="average-container">
+// 			<div className="player-top-container">
+// 				<div className="item-player-top">Total</div>
+// 				<div className="item-player-top">33-25</div>
+// 			</div>
+// 			<div className="item-average">KDRA</div>
+// 			<div className="item-average">1</div>
+// 			<div className="item-average">2</div>
+// 			<div className="item-average">3</div>
+// 			<div className="item-average">4</div>
+// 			<div className="item-average">5</div>
+// 		</div>
+// 	</div>
+// </div>
+// <script src="js/database.js"></script>
+
+
+//                                                   ********************************
+//
+// To add a child to the beginning of a parent, use prepend
+//
+// parent.prepend(newChild)
+// To add at the end of a parent, use append
+//
+// parent.append(newChild)
+//
+
+
+
+
+//
+// function myFunction() {
+// 	var element = document.getElementById("myDIV");
+// 	element.classList.add("mystyle");              *******************************
+// }
+//
+
+
+// <script>                                     **************************
+// 	const para = document.createElement("p");
+// 	const node = document.createTextNode("This is new.");
+// 	para.appendChild(node);
+//
+// 	const element = document.getElementById("div1");
+// 	const child = document.getElementById("p1");
+// 	element.insertBefore(para, child);
+// </script>
+
+
+//////////////// notes/////////////
+//   append 1st, then the text node
+
+
+createGameContainer(`3-21`, `VGG`, `Lose`);
+function createGameContainer(date, opponent, outcome){
+	let frag =document.createDocumentFragment();
+	let body = document.getElementById(`body`);
+	let gameContainer = document.createElement(`div`);
+	gameContainer.classList.add(`game-container`)
+	frag.append(gameContainer);
+	let matchContainer = document.createElement(`div`)
+	matchContainer.classList.add(`match-container`);
+	gameContainer.appendChild(matchContainer);
+	let matchC1 = document.createElement(`div`)
+	matchC1.classList.add(`item-match-info`);
+	matchContainer.appendChild(matchC1)
+	let dateNode = document.createTextNode(`${date}`);
+	matchC1.appendChild(dateNode);
+	let matchC2 = document.createElement(`div`)
+	matchC2.classList.add(`item-match-info`);
+	matchContainer.appendChild(matchC2)
+	let againstNode = document.createTextNode(`Against: ${opponent}`);
+	matchC2.appendChild(againstNode);
+	let matchC3 = document.createElement(`div`)
+	matchC3.classList.add(`item-match-info`);
+	if(outcome === `Lose`){
+		matchC3.classList.add(`red`);
+	}
+	matchContainer.appendChild(matchC3)
+	let outcomeNode = document.createTextNode(`${outcome}`);
+	matchC3.appendChild(outcomeNode);
+	
+	
+	
+	/////always at the END!!!!!!
+	body.append(frag);
+	
+}
+
+
+// function createMatchContainer(){
+// 	let newDiv = document.createElement(`div`);
+// 	newDiv.classList.add(`match-container`)
+// 	let body = document.getElementById(`game-container`);
+// 	body.append(newDiv);
+//
+// }
+
